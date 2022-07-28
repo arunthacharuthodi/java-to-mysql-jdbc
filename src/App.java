fcac8504bd4867091eec2e3e4077969033109f57
Arun Thacharuthodi <67404902+arunthacharuthodi@users.noreply.github.com>
2022-07-28 21:43:46 +0530

    Update App.java

--- src/App.java
+++ src/App.java
@@ -6,14 +6,20 @@ import java.sql.Statement;
 public class App {
     public static void main(String[] args) throws Exception {
         //connect to mysql database
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/arun?allowPublicKeyRetrieval=true&useSSL=false", "aruntest", "Test@123");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?allowPublicKeyRetrieval=true&useSSL=false", "eelab", "Eelab@123");
         //create statement
         Statement stmt = conn.createStatement();
         //execute query
-        ResultSet rs = stmt.executeQuery("select * from class");
+        //stmt.executeQuery("create table test(name varchar(20), rollnumber varchar(20));");
+        //stmt.executeQuery("insert into to test(name , rollnumber) values('myname', 'myname@123');");
+        ResultSet rs = stmt.executeQuery("insert into to test(name , rollnumber) values('myname', 'myname@123');");
+        
+        
+        
+        //ResultSet rs = stmt.executeQuery("select * from test");
         //process result set
         while (rs.next()) {
-            System.out.println(rs.getString(1) + " " + rs.getString(2)+ " " + rs.getString(3)+ " " + rs.getString(4));
+            System.out.println(rs.getString(1) + " " + rs.getString(2));
         }
         //close connection
         conn.close();
